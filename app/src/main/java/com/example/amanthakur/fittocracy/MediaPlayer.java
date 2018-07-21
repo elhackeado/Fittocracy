c0a90fa7c1d33d623644953d9dd9ed59bea1c43a
elhackeado <theelhackeado@gmail.com>
2018-07-22 01:37:17 +0530

    Added Functionality of TIMER

--- app/src/main/java/com/example/amanthakur/fittocracy/MediaPlayer.java
+++ app/src/main/java/com/example/amanthakur/fittocracy/MediaPlayer.java
@@ -1,18 +1,28 @@
 package com.example.amanthakur.fittocracy;
 
 import android.net.Uri;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.net.URI;
 
 public class MediaPlayer extends AppCompatActivity {
-
-
+TextView textView,textView2,textView3,textView4,textView5;
+Button addbtn,subtractbtn,startbtn;
+EditText timerInputET ;
+ImageButton imgbtn;
+ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,29 +35,138 @@ public class MediaPlayer extends AppCompatActivity {
         String name = b.getString("name");
 
         getSupportActionBar().setTitle(name);
+
+        textView = (TextView) findViewById(R.id.textView5);
+        textView2 = (TextView) findViewById(R.id.textView6);
+        textView3 = (TextView) findViewById(R.id.textView7);
+        textView4 = (TextView) findViewById(R.id.textView8);
+        textView4.setText(name);
+        textView5 = (TextView) findViewById(R.id.textView9);
+
+        timerInputET = (EditText) findViewById(R.id.timerInput);
+
+        addbtn = (Button) findViewById(R.id.add);
+        subtractbtn = (Button) findViewById(R.id.subtract);
+        startbtn = (Button) findViewById(R.id.start);
+
+        imgView = (ImageView) findViewById(R.id.imageView3);
+
+        imgbtn = (ImageButton) findViewById(R.id.imageButton);
+
         //Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + (String)(b.getCharSequence("address")));
 
         final VideoView videoView =(VideoView)findViewById(R.id.videoView);
 
         //Creating MediaController
-        MediaController mediaController= new MediaController(this);
-        mediaController.setAnchorView(videoView);
+        //final MediaController mediaController= new MediaController(this);
+        //mediaController.setAnchorView(videoView);
 
         //specify the location of media file
-         Uri uri=Uri.parse("android.resource://" + getPackageName() + "/"
+         final Uri uri=Uri.parse("android.resource://" + getPackageName() + "/"
                 + id );
 
+
+         addbtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 int input = Integer.parseInt(timerInputET.getText().toString().trim());
+                 if(input<600){
+                     timerInputET.setText(""+(input+10));
+                 }
+                 else {
+                     Toast.makeText(MediaPlayer.this,"MAX TIME LIMIT EXCEEDED",Toast.LENGTH_LONG).show();
+                 }
+             }
+         });
+
+         subtractbtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 int input = Integer.parseInt(timerInputET.getText().toString().trim());
+
+                 if(input>20){
+                     timerInputET.setText("" + (input-10));
+                 }
+                 else {
+                     Toast.makeText(MediaPlayer.this,"LEAST LIMIT : 20 SECONDS ",Toast.LENGTH_LONG).show();
+                 }
+             }
+         });
+
+         startbtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                    int input = Integer.parseInt(timerInputET.getText().toString());
+
+                    if(input<=600 && input>=20) {
+                        textView.setVisibility(View.GONE);
+                        textView2.setVisibility(View.GONE);
+                        textView3.setVisibility(View.GONE);
+                        textView4.setVisibility(View.GONE);
+                        timerInputET.setVisibility(View.GONE);
+                        addbtn.setVisibility(View.GONE);
+                        subtractbtn.setVisibility(View.GONE);
+                        startbtn.setVisibility(View.GONE);
+
+                        new CountDownTimer(input*1000, 1000) { // Here I want to have a user input value in milli seconds instead of pre loaded value of 30000
+
+                            public void onTick(long millisUntilFinished) {
+                                textView5.setText("" + millisUntilFinished / 1000);
+                            }
+
+                            public void onFinish() {
+                                textView5.setText("TIME'S UP!!");
+                                imgbtn.setVisibility(View.VISIBLE);
+                                // Turning on the button when the timer has finnished
+                                //mButton.setEnabled(true);
+                                //mButton.setClickable(true);
+                                videoView.stopPlayback();
+                            }
+                        }.start();
+
+                        textView5.setVisibility(View.VISIBLE);
+                        imgView.setVisibility(View.VISIBLE);
+                        videoView.setVisibility(View.VISIBLE);
+
+                        //videoView.setMediaController(mediaController);
+                        videoView.setVideoURI(uri);
+                        videoView.requestFocus();
+                        videoView.start();
+                        videoView.setOnCompletionListener(new android.media.MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(android.media.MediaPlayer mediaPlayer) {
+                                videoView.start();
+                            }
+                        });
+                    }
+                    else{
+                        Toast.makeText(MediaPlayer.this,"Input 20-600 Seconds",Toast.LENGTH_LONG).show();
+                    }
+             }
+         });
         //Setting MediaController and URI, then starting the videoView
-        videoView.setMediaController(mediaController);
-        videoView.setVideoURI(uri);
-        videoView.requestFocus();
-        videoView.start();
-        videoView.setOnCompletionListener(new android.media.MediaPlayer.OnCompletionListener() {
+        imgbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCompletion(android.media.MediaPlayer mediaPlayer) {
-                videoView.start();
+            public void onClick(View view) {
+                textView.setVisibility(View.VISIBLE);
+                textView2.setVisibility(View.VISIBLE);
+                textView3.setVisibility(View.VISIBLE);
+                textView4.setVisibility(View.VISIBLE);
+                timerInputET.setVisibility(View.VISIBLE);
+                addbtn.setVisibility(View.VISIBLE);
+                subtractbtn.setVisibility(View.VISIBLE);
+                startbtn.setVisibility(View.VISIBLE);
+                imgView.setVisibility(View.VISIBLE);
+
+                videoView.setVisibility(View.GONE);
+                textView5.setVisibility(View.GONE);
+                imgbtn.setVisibility(View.GONE);
+
+
+
             }
         });
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
